78fa5d5c2e0a8204ad5a30518598ff4d0ec27e9f
Marco Behler <marco@marcobehler.com>
2018-03-02 10:08:07 +0100

    added the kitten

---
+++ src/main/java/com/marcobehler/Kitten.java
@@ -0,0 +1,11 @@
+package com.marcobehler;
+
+/**
+ * Thanks for watching this episode! Send any feedback to info@marcobehler.com!
+ */
+public class Kitten {
+
+    public void bla() {
+        System.out.println("blah!");
+    }
+}
