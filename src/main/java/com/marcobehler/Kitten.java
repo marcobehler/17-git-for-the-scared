45203648b6ee9e1a08f230e65567bfdd9fd5fbd9
Marco Behler <marco@marcobehler.com>
2018-03-16 10:45:39 +0100

    asda

--- src/main/java/com/marcobehler/Kitten.java
+++ src/main/java/com/marcobehler/Kitten.java
@@ -1,22 +1,12 @@
 package com.marcobehler;
 
-import org.apache.commons.lang.RandomStringUtils;
-
 /**
  * Thanks for watching this episode! Send any feedback to info@marcobehler.com!
  */
 public class Kitten {
 
-    public void meow() {
-        System.out.println("meoooowww" + RandomStringUtils.randomAlphabetic(2));
-    }
-
     public void blah() {
-        System.out.println("eat!");
+        System.out.println("blah!");
         System.out.println("change");
-    }
-
-    public void hopOnKeyboard() {
-        System.out.println("hopping");
     }
 }
