74711fcff70f58def37b7b90f5e91790dd3e9f79
Marco Behler <marco@marcobehler.com>
2018-03-16 10:52:39 +0100

    jumping

--- src/main/java/com/marcobehler/Kitten.java
+++ src/main/java/com/marcobehler/Kitten.java
@@ -1,26 +1,12 @@
 package com.marcobehler;
 
-import org.apache.commons.lang.RandomStringUtils;
-
 /**
  * Thanks for watching this episode! Send any feedback to info@marcobehler.com!
  */
 public class Kitten {
 
-    public void meow() {
-        System.out.println("meeoooowww" + RandomStringUtils.randomAlphabetic(20));
-    }
-
     public void blah() {
         System.out.println("blah!");
         System.out.println("more blah!");
-    }
-
-    public void jump() {
-        System.out.println("I am jumping!!");
-    }
-
-    public void hopOnKeyboard() {
-        System.out.println("hopping on your keyboard!");
     }
 }
