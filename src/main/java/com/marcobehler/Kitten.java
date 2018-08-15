ceda3fe337c844cbc513782160e857db53353aa7
Marco Behler <marco@marcobehler.com>
2018-08-15 15:51:42 +0200

    improved kittens

--- src/main/java/com/marcobehler/Kitten.java
+++ src/main/java/com/marcobehler/Kitten.java
@@ -2,8 +2,6 @@ package com.marcobehler;
 
 import org.apache.commons.lang.RandomStringUtils;
 
-import java.io.File;
-
 /**
  * Thanks for watching this episode! Send any feedback to info@marcobehler.com!
  */
@@ -15,16 +13,6 @@ public class Kitten {
 
     public void meow() {
         System.out.println("Meeooooww" + RandomStringUtils.randomAlphabetic(20));
-    }
-
-public void test() {
-	System.out.println("This is some test....");
-}
-
-
-    public void joho() {
-        File v = new File("jo");
-        System.out.println(v);
     }
 
     public void blah() {
