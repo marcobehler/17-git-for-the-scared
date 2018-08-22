d695644a72f048e17431d1f3d5cec595715c1405
Marco Behler <marco@marcobehler.com>
2018-08-22 08:36:06 +0200

    feature 3

--- src/main/java/com/marcobehler/Kitten.java
+++ src/main/java/com/marcobehler/Kitten.java
@@ -38,6 +38,10 @@ public class Kitten {
         System.out.println("todo");
     }
 
+    public void feature3() {
+        System.out.println("todo ");
+    }
+
     public void bugfix(String todo) {
         System.out.println("todo");
     }
